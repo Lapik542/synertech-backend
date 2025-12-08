d494011a8bfe223b9c3c9475a0d47093c8303906
Гавриш Олександр <sashawwe21@gmail.com>
2025-12-08 14:46:36 +0200

    new google links

--- src/config/env.js
+++ src/config/env.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const env = {
   port: process.env.PORT || 3000,
-  googleScriptUrl: process.env.GOOGLE_SCRIPT_URL || 'https://script.google.com/macros/s/AKfycbwESIt5I576ndnKPrW4nOzAP4nvMjfaeRLXsx0NYcGW8r1VhLLN5ToUf3yNk-V5aDS6iA/exec',
+  googleScriptUrl: process.env.GOOGLE_SCRIPT_URL || 'https://script.google.com/macros/s/AKfycbxnkwtzBChLwnra6qB_YJ_M5LqLc6mdiKxcm2MRnAjx6TtfApfOTNSEoGPZFmzLPpmYRw/exec',
   allowedOrigins: (process.env.ALLOWED_ORIGINS || 'http://localhost:4200')
     .split(',')
     .map(o => o.trim())
