3a9793eb736f00b96f9185035b48b81037f4246b
Гавриш Олександр <sashawwe21@gmail.com>
2025-12-10 10:02:07 +0200

    fix env cors

--- src/config/env.js
+++ src/config/env.js
@@ -2,12 +2,24 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const toArray = (value) => {
+  if (!value) return [];
+  return value
+    .split(',')
+    .map((v) => v.trim())
+    .filter(Boolean);
+};
+
 export const env = {
   port: process.env.PORT || 3000,
-  googleScriptUrl: process.env.GOOGLE_SCRIPT_URL || 'https://script.google.com/macros/s/AKfycbwPsWvJuTaftUmGC985MZVUWubXwWLvtyc4l3gcxVf0R3d9N9_NSXjOhCibL44BAGWGdg/exec',
-  allowedOrigins: (process.env.ALLOWED_ORIGINS || 'http://localhost:4200')
-    .split(',')
-    .map(o => o.trim())
+
+  googleScriptUrl:
+    process.env.GOOGLE_SCRIPT_URL ||
+    'https://script.google.com/macros/s/AKfycbwPsWvJuTaftUmGC985MZVUWubXwWLvtyc4l3gcxVf0R3d9N9_NSXjOhCibL44BAGWGdg/exec',
+
+  allowedOrigins: toArray(
+    process.env.ALLOWED_ORIGINS || 'http://localhost:4200'
+  )
 };
 
 export default env;
